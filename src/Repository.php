9883e361893b1a81da02daee1e21cfe0a497ef23
lingyun <582194733@qq.com>
2020-04-14 11:14:35 +0800

    移除tp-orm model 已经实现的方法

--- src/Repository.php
+++ src/Repository.php
@@ -44,72 +44,13 @@ abstract class Repository implements RepositoryInterface
     abstract public function model();
 
     /**
-     * @param array $columns
-     * @return mixed
-     */
-    public function all($columns = array('*'))
-    {
-        return $this->model->get($columns);
-    }
-
-    /**
-     * @param int $perPage
-     * @param array $columns
-     * @return mixed
-     */
-    public function paginate($perPage = 15, $columns = array('*'))
-    {
-        return $this->model->paginate($perPage, $columns);
-    }
-
-    /**
-     * @param array $data
-     * @return mixed
-     */
-    public function create(array $data)
-    {
-        return $this->model->create($data);
-    }
-
-    /**
-     * @param array $data
-     * @param $id
-     * @param string $attribute
-     * @return mixed
-     */
-    public function update(array $data, $id, $attribute = "id")
-    {
-        return $this->model->where($attribute, '=', $id)->update($data);
-    }
-
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function delete($id)
-    {
-        return $this->model->destroy($id);
-    }
-
-    /**
      * @param $id
      * @param array $columns
      * @return mixed
      */
     public function find($id, $columns = array('*'))
     {
-        return $this->model->find($id, $columns);
-    }
-
-    /**
-     * @param $attribute
-     * @param $value
-     * @param array $columns
-     * @return mixed
-     */
-    public function findBy($attribute, $value, $columns = array('*'))
-    {
-        return $this->model->where($attribute, '=', $value)->first($columns);
+        return $this->model->field($columns)->find($id);
     }
 
     /**
