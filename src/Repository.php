384cd0276fb993626238e2697827c21a22f4649b
lingyun <582194733@qq.com>
2020-05-14 13:58:57 +0800

    加入数据验证方法

--- src/Repository.php
+++ src/Repository.php
@@ -9,6 +9,7 @@ use think\Container;
 use think\helper\Str;
 use think\Model;
 use think\Request;
+use think\Validate;
 
 /**
  * Class Repository
@@ -102,5 +103,41 @@ abstract class Repository implements RepositoryInterface
             throw new RepositoryException("Class {$this->model()} must be an instance of think\\Model");
         }
         return $this->model = $model; //->newInstance();
+    }
+    /**
+     * 验证数据
+     * @access protected
+     * @param  array        $data     数据
+     * @param  string|array $validate 验证器名或者验证规则数组
+     * @param  array        $message  提示信息
+     * @param  bool         $batch    是否批量验证
+     * @return array|string|true
+     * @throws ValidateException
+     */
+    protected function validate(array $data, $validate, array $message = [], bool $batch = false)
+    {
+        if (is_array($validate)) {
+            $v = new Validate();
+            $v->rule($validate);
+        } else {
+            if (strpos($validate, '.')) {
+                // 支持场景
+                [$validate, $scene] = explode('.', $validate);
+            }
+            $class = false !== strpos($validate, '\\') ? $validate : $this->app->parseClass('validate', $validate);
+            $v     = new $class();
+            if (!empty($scene)) {
+                $v->scene($scene);
+            }
+        }
+
+        $v->message($message);
+
+        // 是否批量验证
+        if ($batch || $this->batchValidate) {
+            $v->batch(true);
+        }
+
+        return $v->failException(true)->check($data);
     }
 }
